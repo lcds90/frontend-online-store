f374377fe85f7cb5c671c28f364189416ff649ec
Leonardo Santos <lcds90@gmail.com>
2021-08-11 19:36:20 -0300

    staged index

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,5 @@ export { default as CategoriesList } from './CategoriesList';
 export { default as ProductCard } from './ProductCard';
 export { default as ProductList } from './ProductList';
 export { default as SearchBar } from './SearchBar';
-<<<<<<< HEAD
 export { default as Form } from './Form';
-=======
 export { default as ProductCart } from './ProductCart';
->>>>>>> f7681e7a8cb0a9a9e5e6de48e3cff229a2f81a9e
